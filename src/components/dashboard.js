7e149fc562d7a9d81a6ff5a87e5fc98d6bb1b394
Kari Bancroft <kariabancroft@gmail.com>
2018-01-11 12:11:40 -0800

    fix issue with title and the dashboard items being on the same line

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,14 +4,14 @@ import { Row, Callout, Colors, Alignments } from 'react-foundation';
 
  const Dashboard = (props) => {
    return (
-     <Row>
-       <p><h3>Welcome to your dashboard!</h3></p>
-       <div >
+     <div>
+       <h3>Welcome to your dashboard!</h3>
+       <div>
          <Callout alignment={Alignments.RIGHT} color={ Colors.SECONDARY }>
            <p>You have { props.items } total items today</p>
          </Callout>
        </div>
-     </Row>
+     </div>
    );
 };
 
